cff77f6ebd0ef69175cadb9e525d8a9cba1216e2
asma828 <asmalachhab66@gmail.com>
2025-11-25 16:53:46 +0100

    crée l'entity user

--- src/main/java/com/example/SmartShop/Entity/User.java
+++ src/main/java/com/example/SmartShop/Entity/User.java
@@ -1,5 +1,27 @@
 package com.example.SmartShop.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import com.example.SmartShop.enums.UserRole;
 
-
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
