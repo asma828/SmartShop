ffc2e629687fdb400e3ece49ee8f1d04af73f1e3
asma828 <asmalachhab66@gmail.com>
2025-11-26 11:46:19 +0100

    ajouter une methode setUser pour sauvgarder user objet dans la session apres l'authentification

--- src/main/java/com/example/SmartShop/util/SessionUtil.java
+++ src/main/java/com/example/SmartShop/util/SessionUtil.java
@@ -1,4 +1,17 @@
 package com.example.SmartShop.util;
 
+import com.example.SmartShop.Entity.User;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpSession;
+
 public class SessionUtil {
+    private static final String USER_SESSION_KEY = "LOGGED_IN_USER";
+
+    // sauvgarder l'utilisateur dans la session
+    public static void setUser(HttpSession session, User user){
+        session.setAttribute(USER_SESSION_KEY,user);
+    }
+
+
 }
