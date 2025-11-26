8f0436d24dbdeaffbb8d37177c8891147ef6ad8b
asma828 <asmalachhab66@gmail.com>
2025-11-26 09:44:52 +0100

    ajouter une methode dans le repository pour voir l'existence d'une utilisateur par son nom

--- src/main/java/com/example/SmartShop/dto/request/LoginRequest.java
+++ src/main/java/com/example/SmartShop/dto/request/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.example.SmartShop.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
 }
