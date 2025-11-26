ae7adc18d046e61e37fe4ecb381960c5eb7a669b
asma828 <asmalachhab66@gmail.com>
2025-11-26 11:56:41 +0100

    ajouter une methode pour verifier que l'utilisateur est connecte

--- src/main/java/com/example/SmartShop/util/SessionUtil.java
+++ src/main/java/com/example/SmartShop/util/SessionUtil.java
@@ -14,12 +14,18 @@ public class SessionUtil {
         session.setAttribute(USER_SESSION_KEY,user);
     }
 
+    // Récupérer l'utilisateur de la session
     public static User getUser(HttpSession session){
         Object user = session.getAttribute(USER_SESSION_KEY);
         if(user == null){
             throw new UnauthorizedException("No user logged in , please log in first");
         }
         return (User) user;
+    }
+
+    // Vérifier si l'utilisateur est connecté
+    public static boolean isAuthenficated(HttpSession session){
+        return session.getAttribute(USER_SESSION_KEY)!= null;
     }
 
 
