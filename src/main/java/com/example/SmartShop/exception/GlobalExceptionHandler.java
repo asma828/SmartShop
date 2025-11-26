932ffee54cbe2876ae8eec085b7ec7ac59dc05e4
asma828 <asmalachhab66@gmail.com>
2025-11-26 10:56:22 +0100

    handle l'exception de unauthorized avec l'error response

--- src/main/java/com/example/SmartShop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/SmartShop/exception/GlobalExceptionHandler.java
@@ -1,7 +1,28 @@
 package com.example.SmartShop.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request){
+    ErrorResponse error = ErrorResponse.builder()
+            .timestamp(LocalDateTime.now())
+            .status(HttpStatus.UNAUTHORIZED.value())
+            .error("Unauthorized")
+            .message(ex.getMessage())
+            .path(request.getRequestURI())
+            .build();
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+
+
+
+    }
 }
