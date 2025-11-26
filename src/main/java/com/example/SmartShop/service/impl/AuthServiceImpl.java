f7707583f02a52d453eaebe50a193f9373e48e63
asma828 <asmalachhab66@gmail.com>
2025-11-26 16:04:27 +0100

    ajouter la recherche d'un utilisateur et la verification de password a l'implementation de la methode login

--- src/main/java/com/example/SmartShop/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/SmartShop/service/impl/AuthServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.SmartShop.service.impl;
 
+import com.example.SmartShop.Entity.User;
 import com.example.SmartShop.dto.request.LoginRequest;
+import com.example.SmartShop.dto.response.AuthResponse;
+import com.example.SmartShop.exception.UnauthorizedException;
 import com.example.SmartShop.repository.UserRepository;
 import com.example.SmartShop.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +17,14 @@ public class AuthServiceImpl implements AuthService {
     private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
-    public AuthService login(LoginRequest request, HttpSession session){
-        log.info("login attempt for username :{}",request.getUsername() );
+    public AuthResponse login(LoginRequest request, HttpSession session){
+        log.info("login attempt for username :{}",request.getUsername());
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(()-> new UnauthorizedException("invalide username or password"));
+        if(passwordEncoder.matches(request.getPassword(),user.getPassword())){
+            log.warn("Invalide password for username: {}",request.getUsername());
+            throw new UnauthorizedException("Invalide username or password");
+        }
     }
+
 }
