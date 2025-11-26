48dfd90316cfec1c44849d671fa1a89870abc805
asma828 <asmalachhab66@gmail.com>
2025-11-26 09:36:19 +0100

    crée PaymentType enum pour les types de paiement

---
+++ src/main/java/com/example/SmartShop/enums/PaymentType.java
@@ -0,0 +1,7 @@
+package com.example.SmartShop.enums;
+
+public enum PaymentType {
+    ESPECES,   // Limite légale : 20,000 DH
+    CHEQUE,    // Peut être différé
+    VIREMENT   // Paiement immédiat ou différé
+}
