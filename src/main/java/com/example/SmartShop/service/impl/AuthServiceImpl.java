5a31ac9c94b7cf1dafeca11803592654f35a9154
asma828 <asmalachhab66@gmail.com>
2025-11-26 15:52:51 +0100

    implementer la methode login de authservice interface avec l'affichage d'un message dans le console

--- src/main/java/com/example/SmartShop/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/SmartShop/service/impl/AuthServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.SmartShop.service.impl;
 
-public class AuthServiceImpl {
+import com.example.SmartShop.dto.request.LoginRequest;
+import com.example.SmartShop.repository.UserRepository;
+import com.example.SmartShop.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public AuthService login(LoginRequest request, HttpSession session){
+        log.info("login attempt for username :{}",request.getUsername() );
+    }
 }
