e8a57b172b46b4e05edc73367fa3be82b3e07914
asma828 <asmalachhab66@gmail.com>
2025-11-26 15:20:27 +0100

    ajouter une methode au service pour la proccess de connexion

--- src/main/java/com/example/SmartShop/service/AuthService.java
+++ src/main/java/com/example/SmartShop/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.example.SmartShop.service;
 
-public class AuthService {
+import com.example.SmartShop.dto.request.LoginRequest;
+import com.example.SmartShop.dto.response.AuthResponse;
+
+import javax.servlet.http.HttpSession;
+
+public interface AuthService {
+    AuthResponse login(LoginRequest request, HttpSession session);
 }
