dde5432df3d2d236a81a6b2bcb81b8855f0080c6
asma828 <asmalachhab66@gmail.com>
2025-11-26 10:57:32 +0100

    changes le type de status et de timestamp

--- src/main/java/com/example/SmartShop/exception/ErrorResponse.java
+++ src/main/java/com/example/SmartShop/exception/ErrorResponse.java
@@ -1,15 +1,19 @@
 package com.example.SmartShop.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ErrorResponse {
-    private String timestamp;
-    private String status;
+    private LocalDateTime timestamp;
+    private int status;
     private String error;
     private String message;
     private String path;
