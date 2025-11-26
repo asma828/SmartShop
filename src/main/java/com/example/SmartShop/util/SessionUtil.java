39e65c8b068c8c6705a9aa24bd6ca286adfb4a05
asma828 <asmalachhab66@gmail.com>
2025-11-26 11:51:16 +0100

    ajouter une methode pour receperer l'utilisateur depuis la session

--- src/main/java/com/example/SmartShop/util/SessionUtil.java
+++ src/main/java/com/example/SmartShop/util/SessionUtil.java
@@ -1,6 +1,7 @@
 package com.example.SmartShop.util;
 
 import com.example.SmartShop.Entity.User;
+import com.example.SmartShop.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpSession;
@@ -11,6 +12,14 @@ public class SessionUtil {
     // sauvgarder l'utilisateur dans la session
     public static void setUser(HttpSession session, User user){
         session.setAttribute(USER_SESSION_KEY,user);
+    }
+
+    public static User getUser(HttpSession session){
+        Object user = session.getAttribute(USER_SESSION_KEY);
+        if(user == null){
+            throw new UnauthorizedException("No user logged in , please log in first");
+        }
+        return (User) user;
     }
 
 
