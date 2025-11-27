9dbca636244c65b7f5403a8f595ff976b9f4dbc0
asma828 <asmalachhab66@gmail.com>
2025-11-27 14:52:53 +0100

    crée orderItem response

---
+++ src/main/java/com/example/SmartShop/dto/response/OrderItemResponse.java
@@ -0,0 +1,21 @@
+package com.example.SmartShop.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItemResponse {
+    private Long id;
+    private Long productId;
+    private String productNom;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalLine;
+}
