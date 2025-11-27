87af2b4b9c0d0006158d64cbd0757b8d6901f6cf
asma828 <asmalachhab66@gmail.com>
2025-11-27 12:11:54 +0100

    crée une methode pour checke l'existance d'un client par email

--- src/main/java/com/example/SmartShop/repository/ClientRepository.java
+++ src/main/java/com/example/SmartShop/repository/ClientRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,String> {
     Optional<Client> findByEmail(String email);
+    boolean existeByEmail(String email);
 }
