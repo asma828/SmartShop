729d3040f533dd4a13108e0628b21e9ba8b67b15
asma828 <asmalachhab66@gmail.com>
2025-11-27 10:14:40 +0100

    créé l'entity client

--- src/main/java/com/example/SmartShop/Entity/Client.java
+++ src/main/java/com/example/SmartShop/Entity/Client.java
@@ -1,4 +1,63 @@
 package com.example.SmartShop.Entity;
 
+import com.example.SmartShop.enums.CustomerTier;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.mapstruct.control.MappingControl;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private CustomerTier niveauFidelite = CustomerTier.BASIC;
+
+
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "client",cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Order> orders = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime updateAt;
+
+
+
 }
