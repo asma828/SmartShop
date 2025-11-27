316a52b8be03bf2bb8064a3df977fd69c719f7e7
asma828 <asmalachhab66@gmail.com>
2025-11-27 16:01:59 +0100

    ajouter un methode au service pour crée un client

---
+++ src/main/java/com/example/SmartShop/service/ClientService.java
@@ -0,0 +1,11 @@
+package com.example.SmartShop.service;
+
+import com.example.SmartShop.dto.request.ClientRequest;
+import com.example.SmartShop.dto.response.ClientResponse;
+
+import java.util.List;
+
+public interface ClientService {
+    ClientResponse createClient(ClientRequest request);
+
+}
