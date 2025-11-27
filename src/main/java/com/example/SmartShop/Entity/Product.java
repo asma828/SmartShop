c9d1e27938272bed9ee73d5883c9026b397037d2
asma828 <asmalachhab66@gmail.com>
2025-11-27 10:21:11 +0100

    créé l'entity produit

--- src/main/java/com/example/SmartShop/Entity/Product.java
+++ src/main/java/com/example/SmartShop/Entity/Product.java
@@ -1,4 +1,34 @@
 package com.example.SmartShop.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false,precision = 10,scale = 2)
+    private BigDecimal prix;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer stock=0;
+
+
+
 }
