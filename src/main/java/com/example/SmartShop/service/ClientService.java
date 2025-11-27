9a3a4ae7539a536db83f36a5d8b593edb60e633d
asma828 <asmalachhab66@gmail.com>
2025-11-27 16:03:57 +0100

    ajouter un methode au service pour recupere tout les clients avec pagination

--- src/main/java/com/example/SmartShop/service/ClientService.java
+++ src/main/java/com/example/SmartShop/service/ClientService.java
@@ -2,6 +2,8 @@ package com.example.SmartShop.service;
 
 import com.example.SmartShop.dto.request.ClientRequest;
 import com.example.SmartShop.dto.response.ClientResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -9,4 +11,5 @@ public interface ClientService {
     ClientResponse createClient(ClientRequest request);
     ClientResponse getClientById(Long id);
     List<ClientResponse> getAllClients();
+    Page<ClientResponse> getAllClients(Pageable pageable);
 }
