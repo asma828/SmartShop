1e340bacbe66a5ee7052c5cbdedabc6bd78d990a
asma828 <asmalachhab66@gmail.com>
2025-11-27 11:51:07 +0100

    crée l'entity payement avec la relation avec order et ajouter une status de payement

--- src/main/java/com/example/SmartShop/Entity/Payement.java
+++ src/main/java/com/example/SmartShop/Entity/Payement.java
@@ -1,4 +1,69 @@
 package com.example.SmartShop.Entity;
 
+import com.example.SmartShop.enums.PayementStatus;
+import com.example.SmartShop.enums.PaymentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.*;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payements")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Payement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id",nullable = false)
+    @ToString.Exclude
+    private Order order;
+
+    @Column(nullable = false)
+    private Integer numeroPaiement;
+
+    @Column(nullable = false,precision = 10,scale = 2)
+    private Integer montant;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentType paymentType;
+
+    @Column(nullable = false)
+    private LocalDate datePaiement;
+
+    @Column(nullable = false)
+    private LocalDate dateEncaissement;
+
+    @Column(nullable = false)
+    private LocalDate dateEcheance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PayementStatus payementStatus = PayementStatus.EN_ATTENTE;
+
+    @Column
+    private String reference;
+
+    @Column
+    private String banque;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
+
+
+
+
 }
---
+++ src/main/java/com/example/SmartShop/enums/PayementStatus.java
@@ -0,0 +1,6 @@
+package com.example.SmartShop.enums;
+
+public enum PayementStatus {
+    EN_ATTENTE,
+
+}
