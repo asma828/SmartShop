b9d60db15ccc9199c922f4d5ae98ac98a35f147e
asma828 <asmalachhab66@gmail.com>
2025-11-27 12:19:52 +0100

    crée Payement repositrory

---
+++ src/main/java/com/example/SmartShop/repository/PayementRepository.java
@@ -0,0 +1,7 @@
+package com.example.SmartShop.repository;
+
+import com.example.SmartShop.Entity.Payement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayementRepository extends JpaRepository<Payement,Long> {
+}
