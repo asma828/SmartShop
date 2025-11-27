d50435bc24832ff948b592a0dd57dffdf662bc32
asma828 <asmalachhab66@gmail.com>
2025-11-27 12:14:24 +0100

    crée product repositrory

---
+++ src/main/java/com/example/SmartShop/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.SmartShop.repository;
+
+import com.example.SmartShop.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
