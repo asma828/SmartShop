27e7b77e350db0106ffa0eccfd7d1384c15f1f4c
asma828 <asmalachhab66@gmail.com>
2025-11-27 12:10:37 +0100

    crée client repositrory avec une methode findByEmail

---
+++ src/main/java/com/example/SmartShop/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.example.SmartShop.repository;
+
+import com.example.SmartShop.Entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client,String> {
+    Optional<Client> findByEmail(String email);
+}
