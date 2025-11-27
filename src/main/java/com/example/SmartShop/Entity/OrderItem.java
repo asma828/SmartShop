7058497252dd8d75c74b460301d3843580316719
asma828 <asmalachhab66@gmail.com>
2025-11-27 11:30:39 +0100

    ajouter la relation entre orderItem et order et produit

--- src/main/java/com/example/SmartShop/Entity/OrderItem.java
+++ src/main/java/com/example/SmartShop/Entity/OrderItem.java
@@ -1,4 +1,29 @@
 package com.example.SmartShop.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produit_id",nullable = false)
+    private Product product;
+
+
 }
