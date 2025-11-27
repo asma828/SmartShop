380b1424dde62d86b4edd0efd2c83e54d12c048c
asma828 <asmalachhab66@gmail.com>
2025-11-27 14:52:02 +0100

    crée orderItem request

---
+++ src/main/java/com/example/SmartShop/dto/request/OrderItemRequest.java
@@ -0,0 +1,22 @@
+package com.example.SmartShop.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItemRequest {
+
+    @NotNull(message = "L'ID du produit est obligatoire")
+    private Long productId;
+
+    @NotNull(message = "La quantité est obligatoire")
+    @Min(value = 1, message = "La quantité doit être au moins 1")
+    private Integer quantity;
+}
