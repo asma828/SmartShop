af53db34ce175924d6be09e4bd1de85be0bef7e1
asma828 <asmalachhab66@gmail.com>
2025-11-27 10:52:56 +0100

    ajouter la relation entre order et client

--- src/main/java/com/example/SmartShop/Entity/Order.java
+++ src/main/java/com/example/SmartShop/Entity/Order.java
@@ -1,4 +1,23 @@
 package com.example.SmartShop.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id",nullable = false)
+    private Client client;
 }
