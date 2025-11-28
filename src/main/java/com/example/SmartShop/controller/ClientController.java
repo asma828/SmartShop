b56351d9ddafbf1734c73d3755d712f659f06bcb
asma828 <asmalachhab66@gmail.com>
2025-11-28 11:55:30 +0100

    crée client controller avec la methode de creation

--- src/main/java/com/example/SmartShop/controller/ClientController.java
+++ src/main/java/com/example/SmartShop/controller/ClientController.java
@@ -1,4 +1,29 @@
 package com.example.SmartShop.controller;
 
+import com.example.SmartShop.Entity.Client;
+import com.example.SmartShop.dto.request.ClientRequest;
+import com.example.SmartShop.dto.response.ClientResponse;
+import com.example.SmartShop.service.ClientService;
+import com.example.SmartShop.util.SessionUtil;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/clients")
+@RequiredArgsConstructor
 public class ClientController {
+    private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> createClient(@Valid @RequestBody ClientRequest request, HttpSession session){
+        ClientResponse clientResponse = clientService.createClient(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientResponse);
+    }
 }
