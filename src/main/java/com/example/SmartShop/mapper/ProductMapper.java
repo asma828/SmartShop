3a307910065a1f68075ea015c085ab598fbf7016
asma828 <asmalachhab66@gmail.com>
2025-11-28 10:55:32 +0100

    implementer createProduct methode in the serviceImpl

--- src/main/java/com/example/SmartShop/mapper/ProductMapper.java
+++ src/main/java/com/example/SmartShop/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ import com.example.SmartShop.Entity.Client;
 import com.example.SmartShop.Entity.Product;
 import com.example.SmartShop.dto.request.ClientRequest;
 import com.example.SmartShop.dto.request.ProductRequest;
+import com.example.SmartShop.dto.response.ProductResponse;
 
 public class ProductMapper {
 
@@ -16,6 +17,21 @@ public class ProductMapper {
                 .nom(request.getNom())
                 .prix(request.getPrix())
                 .stock(request.getStock())
+                .build();
+    }
+
+    public ProductResponse toResponse(Product product){
+        if(product == null){
+            return null;
+        }
+
+        return ProductResponse.builder()
+                .id(product.getId())
+                .nom(product.getNom())
+                .prix(product.getPrix())
+                .stock(product.getStock())
+                .createdAt(product.getCreatedAt())
+                .updatedAt(product.getUpdateAt())
                 .build();
     }
 }
--- src/main/java/com/example/SmartShop/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/SmartShop/service/impl/ProductServiceImpl.java
@@ -1,4 +1,24 @@
 package com.example.SmartShop.service.impl;
 
-public class ProductServiceImpl {
+import com.example.SmartShop.Entity.Product;
+import com.example.SmartShop.dto.request.ProductRequest;
+import com.example.SmartShop.dto.response.ProductResponse;
+import com.example.SmartShop.mapper.ProductMapper;
+import com.example.SmartShop.repository.ProductRepository;
+import com.example.SmartShop.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Override
+    public ProductResponse createProduct(ProductRequest request){
+        Product product = productMapper.toEntity(request);
+        Product savedProduct = productRepository.save(product);
+         return productMapper.toResponse(savedProduct);
+    }
 }
