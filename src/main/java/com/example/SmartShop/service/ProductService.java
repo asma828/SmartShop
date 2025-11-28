26ef9a471da015c0a638217bef49a4a20cc71710
asma828 <asmalachhab66@gmail.com>
2025-11-28 09:17:04 +0100

    ajouter un methode au ProduitServiceImpl pour chercher un produit par id

--- src/main/java/com/example/SmartShop/service/ProductService.java
+++ src/main/java/com/example/SmartShop/service/ProductService.java
@@ -4,6 +4,9 @@ import com.example.SmartShop.dto.request.ProductRequest;
 import com.example.SmartShop.dto.response.PaymentResponse;
 import com.example.SmartShop.dto.response.ProductResponse;
 
+import java.util.List;
+
 public interface ProductService {
     ProductResponse createProduct(ProductRequest request);
+    ProductResponse findProductById(Long id);
 }
