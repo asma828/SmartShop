6e5dab8967f2810cca23cae8f2836bd4cc94bde6
asma828 <asmalachhab66@gmail.com>
2025-11-28 12:11:14 +0100

    crée client controller avec la methode pour recuperer un client par id

--- src/main/java/com/example/SmartShop/controller/ClientController.java
+++ src/main/java/com/example/SmartShop/controller/ClientController.java
@@ -10,10 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/clients")
@@ -25,5 +22,12 @@ public class ClientController {
     public ResponseEntity<ClientResponse> createClient(@Valid @RequestBody ClientRequest request, HttpSession session){
         ClientResponse clientResponse = clientService.createClient(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(clientResponse);
+    }
+
+    @GetMapping("/id")
+    public  ResponseEntity<ClientResponse> findClientById(@PathVariable Long id,HttpSession session){
+        SessionUtil.getUser(session);
+        ClientResponse response = clientService.getClientById(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(response);
     }
 }
