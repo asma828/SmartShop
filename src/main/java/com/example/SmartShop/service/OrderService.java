3df7d5512ad87cf02d7a50deddc42dcae5569df2
asma828 <asmalachhab66@gmail.com>
2025-11-29 15:44:07 +0100

    ajouter findByStatus methode a order service

--- src/main/java/com/example/SmartShop/service/OrderService.java
+++ src/main/java/com/example/SmartShop/service/OrderService.java
@@ -2,10 +2,12 @@ package com.example.SmartShop.service;
 
 import com.example.SmartShop.dto.request.OrderRequest;
 import com.example.SmartShop.dto.response.OrderResponse;
+import com.example.SmartShop.enums.OrderStatus;
 
 public interface OrderService {
 OrderResponse createOrder(OrderRequest request);
 OrderResponse findOrderById(Long id);
 OrderResponse findAllOrders();
 OrderResponse getOrderByClient(Long clientId);
+OrderResponse getOrderByStatus(OrderStatus status);
 }
