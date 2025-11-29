08faacf013240d34551eb57f163a10888684eeb3
asma828 <asmalachhab66@gmail.com>
2025-11-29 19:45:15 +0100

    ajouter une methode a productService pour recuperer les produit qui ne sont pas supprimer

--- src/main/java/com/example/SmartShop/repository/ProductRepository.java
+++ src/main/java/com/example/SmartShop/repository/ProductRepository.java
@@ -2,6 +2,11 @@ package com.example.SmartShop.repository;
 
 import com.example.SmartShop.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    @Query("SELECT p FROM Product p WHERE p.id = :id AND p.deleted = false")
+    Optional<Product> findActiveById(Long id);
 }
