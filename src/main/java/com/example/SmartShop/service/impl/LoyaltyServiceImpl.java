4405f8f0bc5eae78f719cd7023a16a3bb1d66803
asma828 <asmalachhab66@gmail.com>
2025-11-30 23:03:42 +0100

    implementer calculerTier dans LoyaltyServiceImpl

--- src/main/java/com/example/SmartShop/service/impl/LoyaltyServiceImpl.java
+++ src/main/java/com/example/SmartShop/service/impl/LoyaltyServiceImpl.java
@@ -1,4 +1,78 @@
 package com.example.SmartShop.service.impl;
 
-public class LoyaltyServiceImpl {
+import com.example.SmartShop.Entity.Client;
+import com.example.SmartShop.enums.CustomerTier;
+import com.example.SmartShop.service.LoyaltyService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.math.BigDecimal;
+
+
+@Service
+public class LoyaltyServiceImpl implements LoyaltyService {
+
+    // Seuils pour obtenir un niveau (basés sur l'historique)
+    @Value("${business.loyalty.silver.orders:3}")
+    private Integer silverOrdersThreshold;
+
+    @Value("${business.loyalty.silver.amount:1000}")
+    private Integer silverAmountThreshold;
+
+    @Value("${business.loyalty.gold.orders:10}")
+    private Integer goldOrdersThreshold;
+
+    @Value("${business.loyalty.gold.amount:5000}")
+    private Integer goldAmountThreshold;
+
+    @Value("${business.loyalty.platinum.orders:20}")
+    private Integer platinumOrdersThreshold;
+
+    @Value("${business.loyalty.platinum.amount:15000}")
+    private Integer platinumAmountThreshold;
+
+    // Remises et seuils pour utiliser un niveau (sur futures commandes)
+    @Value("${business.discount.silver.rate:5}")
+    private Integer silverDiscountRate;
+
+    @Value("${business.discount.silver.threshold:500}")
+    private Integer silverMinimumPurchase;
+
+    @Value("${business.discount.gold.rate:10}")
+    private Integer goldDiscountRate;
+
+    @Value("${business.discount.gold.threshold:800}")
+    private Integer goldMinimumPurchase;
+
+    @Value("${business.discount.platinum.rate:15}")
+    private Integer platinumDiscountRate;
+
+    @Value("${business.discount.platinum.threshold:1200}")
+    private Integer platinumMinimumPurchase;
+    @Override public CustomerTier calculeTier(Client client){
+
+        Integer totalOrder = client.getTotalOrders();
+        BigDecimal totalSpend = client.getTotalSpent();
+
+        // PLATINUM : 20 commandes OU 15,000 DH
+        if(totalOrder >=platinumOrdersThreshold ||
+                totalSpend.compareTo(BigDecimal.valueOf(platinumAmountThreshold)) >= 0){
+            return CustomerTier.PLATINUM;
+        }
+
+        // GOLD : 10 commandes OU 5,000 DH
+        if(totalOrder >=goldOrdersThreshold ||
+                totalSpend.compareTo(BigDecimal.valueOf(goldAmountThreshold)) >= 0){
+            return CustomerTier.GOLD;
+        }
+
+        // SILVER : 3 commandes OU 1,000 DH
+        if(totalOrder >=silverOrdersThreshold ||
+                totalSpend.compareTo(BigDecimal.valueOf(silverAmountThreshold)) >=0){
+            return CustomerTier.SILVER;
+        }
+
+        return CustomerTier.BASIC;
+
+    }
 }
