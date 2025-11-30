e751e048440d3ad1040dbb28c3092c167fced88a
asma828 <asmalachhab66@gmail.com>
2025-11-30 16:03:50 +0100

    ajouter une methode au orderRepository pour trouver une commande par status

--- src/main/java/com/example/SmartShop/repository/OrderRepository.java
+++ src/main/java/com/example/SmartShop/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.example.SmartShop.repository;
 
 import com.example.SmartShop.Entity.Order;
+import com.example.SmartShop.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
     List<Order> findByClientId(Long clientId);
+    List<Order> findByStatus(OrderStatus status);
 }
