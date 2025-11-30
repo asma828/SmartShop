314a9ac3d39a3adf339ea739f8e15091161048e7
asma828 <asmalachhab66@gmail.com>
2025-11-30 14:44:19 +0100

    ajouter une methode au orderRepository pour recupere un client par id

--- src/main/java/com/example/SmartShop/repository/OrderRepository.java
+++ src/main/java/com/example/SmartShop/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.example.SmartShop.repository;
 import com.example.SmartShop.Entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findByClientId(Long clientId);
 }
