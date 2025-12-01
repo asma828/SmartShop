1530c3d10dcdf65f9006661fd419d86cb2ee6286
asma828 <asmalachhab66@gmail.com>
2025-12-01 15:55:10 +0100

    ajouter une methode pour calculer de remise

--- src/main/java/com/example/SmartShop/service/LoyaltyService.java
+++ src/main/java/com/example/SmartShop/service/LoyaltyService.java
@@ -3,7 +3,10 @@ package com.example.SmartShop.service;
 import com.example.SmartShop.Entity.Client;
 import com.example.SmartShop.enums.CustomerTier;
 
+import java.math.BigDecimal;
+
 public interface LoyaltyService {
     CustomerTier calculeTier(Client client);
     void updateClientTier(Client client);
+    BigDecimal calculateDiscountRate(CustomerTier tier,BigDecimal sousTotal);
 }
