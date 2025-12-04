8258dd5ca6545c61203a35eff10a75811939c065
asma828 <asmalachhab66@gmail.com>
2025-12-04 12:38:06 +0100

    ajouter une methode a payementController pour recupere un paiement par id

--- src/main/java/com/example/SmartShop/controller/PaymentController.java
+++ src/main/java/com/example/SmartShop/controller/PaymentController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payements")
@@ -18,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
     private final PaymentService paymentService;
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<PaymentResponse> addPayement(@Valid @RequestBody PaymentRequest request){
         PaymentResponse response = paymentService.addPayment(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentResponse> getPaiementById(@PathVariable Long id){
+        PaymentResponse response = paymentService.getPaymentById(id);
+        return ResponseEntity.ok(response);
     }
 }
